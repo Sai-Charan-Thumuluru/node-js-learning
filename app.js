79758c44f535d007290a8b0dae5f1969c641df98
Sai-Charan-Thumuluru <cherry.learning.art63@gmail>
2025-01-28 17:46:53 +0530

    Local modules, export and import

---
+++ app.js
@@ -0,0 +1,4 @@
+const math = require('./math');
+
+console.log(`Sum from main app module ${math.add(2, 7)}`);
+console.log(`Difference from main app module ${math.sub(2, 7)}`);
---
+++ math.js
@@ -0,0 +1,12 @@
+// Local modules
+
+const add = (a, b) => {
+    return a + b;
+}
+
+const sub = (a, b) => {
+    return a - b;
+}
+
+exports.add = add;
+exports.sub = sub;
