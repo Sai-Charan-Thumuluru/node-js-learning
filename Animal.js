95521deeb0c5ca1f846f4834f8f3ec399b850a20
Sai-Charan-Thumuluru <cherry.learning.art63@gmail>
2025-01-29 13:23:32 +0530

    Module caching

---
+++ Animal.js
@@ -0,0 +1,16 @@
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+}
+
+// exports.animal = Animal;
+exports.animal = new Animal('Elephant');
--- app.js
+++ app.js
@@ -2,3 +2,22 @@ const math = require('./math');
 const sc = require('./string-concat');
 
 console.log(sc.add(`Sum from main app module ${math.add(2, 7)}`, `Difference from main app module ${math.sub(2, 7)}`));
+
+// let Animal = require('./Animal').animal;
+// const cow = new Animal('Cow');
+// console.log(cow.getName());
+// cow.setName('Parrot');
+// console.log(cow.getName());
+
+// Animal = require('./Animal').animal;
+// const tiger = new Animal('Tiger');
+// console.log(tiger.getName());
+
+let Animal = require('./Animal').animal;
+console.log(Animal.getName()); // Elephant
+Animal.setName('Parrot');
+
+Animal = require('./Animal').animal;
+console.log(Animal.getName()); // Parrot
+
+// Because of module caching NodeJS remembers the references 
